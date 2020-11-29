ba98ce1167ba79e8e433de91d372c2965f750507
unknown <dokonala.hilda@gmail.com>
2020-11-29 23:25:14 +0100

    02 - Kresleni obrysu obdelnika/ctverce

--- js/graphics.js
+++ js/graphics.js
@@ -4,6 +4,8 @@ const canvas = document.getElementById('canvas');
    umožňuje používat různé grafické atributy a metody. */
 const ctx = canvas.getContext('2d');
 
+/***************************************************************/
+/* 1. Kreslení vyplněných obdélníků */
 /* V atributu fillStyle je nastavena barva výplně */ 
 ctx.fillStyle = '#FF0000';
 /* Vykreslí obdélník s danou výplní a s parametry (pozice x, pozice y, šířka, výška) */
@@ -11,3 +13,10 @@ ctx.fillRect(0, 0, 150, 75);
 /* Je možné použít nejen plnou barvu (rgb), ale i barvu s průhledností (rgba). */
 ctx.fillStyle = 'rgba(255, 0, 0, 0.5)';
 ctx.fillRect(0, 100, 150, 75);
+
+/***************************************************************/
+/* 2. Kreslení obrysu obdélníka/čtverce */
+/* Nastavení barvy obrysu */
+ctx.strokeStyle = 'black';
+/* Vykreslení obdélníku (čtverce) pomocí obrysu */
+ctx.strokeRect(40, 200, 70, 70);
