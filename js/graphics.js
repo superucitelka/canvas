1e33fdf33ea0c166556feb549e417f931cf5b221
unknown <dokonala.hilda@gmail.com>
2020-12-02 10:36:01 +0100

    A02 - Animace - zmena velikosti ctverce

--- js/graphics.js
+++ js/graphics.js
@@ -18,8 +18,35 @@ let square = {
    paint: function() {
       ctx.fillStyle = this.color;
       ctx.fillRect(this.x, this.y, this.size, this.size);      
-   }
+   },
+
+   /* Změna velikosti čtverce */
+   resize: function() {
+      this.size = (this.size < 250) ? this.size + 1 : 0;
+   }   
 }
 
-/* Vykreslení objektu */
-square.paint();
+/* Proměnná pro uložení animačního cyklu */
+let req;
+
+/* Funkce provádějící animaci */
+function animate() {
+   /* Vyčištění plátna */
+   ctx.clearRect(0, 0, canvas.width, canvas.height);
+   /* Změna velikosti objektu */
+   square.resize();
+   /* Vykreslení objektu */
+   square.paint();
+   /* Vyvolání dalšího snímku animace */
+   req = window.requestAnimationFrame(animate);
+}
+
+/* Spuštění animace po kliknutí na tlačítko Start */
+document.getElementById('start').addEventListener('click', function() {
+   window.requestAnimationFrame(animate);
+});
+
+/* Zastavení animace po kliknutí na tlačítko Stop */
+document.getElementById('stop').addEventListener('click', function() {
+   window.cancelAnimationFrame(req);
+});
